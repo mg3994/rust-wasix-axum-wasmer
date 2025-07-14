024091472f75519e4e050c99864fd30a5a40f77c
Manishmg3994 <manishgautammg7@gmail.com>
2025-07-14 07:13:30 -0700

    new second commit

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
- 
+
 #[tokio::main]
 async fn main() {
-    // Building our application with a single Route
+    // Build our application with a single route
     let app = Router::new().route("/", get(handler));
- 
-    // Run the server with hyper on http://127.0.0.1:3000
+
+    // Run server on 127.0.0.1:3000
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on http://{}", addr);
+
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
- 
+
 async fn handler() -> &'static str {
     "Hello, Axum ❤️ WASIX!"
 }
- 
